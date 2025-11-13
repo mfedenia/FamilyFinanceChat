223d20ead6b208b4aaf11be6b83c2210f71972b8
Sunny Shukla <sunnysshukla16@gmail.com>
2025-11-13 15:35:45 -0600

    Fix backend bug

--- grading_feature/backend/main.py
+++ grading_feature/backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
 from logger import logging
 from extract_chats import main as extract_data
 import uvicorn
@@ -9,6 +10,14 @@ logger = logging.getLogger("professor_dashboard")
 DATA_PATH = "data/extracted_chats.json"
 
 app = FastAPI(title = "Professor Dashboard")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def load_data():
@@ -24,7 +33,7 @@ def get_user(user_id):
     data = load_data()
 
     for user in data:
-        if user['email'] == user_id:
+        if user['user_id'] == user_id:
             return user
     return {"error": "User not found"}
 
