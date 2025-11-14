1fba5669a842162dc0c4fcbf2e3b9dccf78dd65f
Sunny Shukla <sunnysshukla16@gmail.com>
2025-11-14 13:24:25 -0600

    Navbar creation

--- grading_feature/frontend/src/App.jsx
+++ grading_feature/frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import UserDetail from './pages/UserDetail'
+import Navbar from './components/Navbar'
 
 export default function App() {
     return (
         <BrowserRouter>
-            
             <div className="dark bg-[#0d0d0d] text-gray-200 min-h-screen">
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/user/:userId' element={<UserDetail />} />
-                </Routes>
+
+                <Navbar />
+
+                <div className="pt-16 px-6">
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/user/:userId' element={<UserDetail />} />
+                    </Routes>
+                </div>
             </div>
         </BrowserRouter>
     )
