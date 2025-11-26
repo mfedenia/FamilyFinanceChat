0adca90b8b6265dc6b49880a1868f34712d50b43
ronaldmishiev <ronaldmishiev@gmail.com>
2025-11-26 06:48:19 -0600

    removed extra files

--- container-backup/static/loader.js
+++
@@ -1,7 +0,0 @@
-// Custom loader
-console.log('[Loader] Loading customizations...');
-if (!document.querySelector('script[src*="custom_button.js"]')) {
-    const script = document.createElement('script');
-    script.src = '/static/custom_button.js?v=' + Date.now();
-    document.head.appendChild(script);
-}
