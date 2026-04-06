47177c1f9efba93ceb083bdd75d2c2b6ad44643c
Amsyar Md Hanif <amsyar1653@gmail.com>
2026-04-05 20:49:06 -0500

    Added beta for user feedback

--- grading_feature/frontend/src/App.jsx
+++ grading_feature/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import UserDetail from './pages/UserDetail'
 import Scoring from './pages/Scoring'
+import StudentFeedback from './pages/StudentFeedback'
 import Navbar from './components/Navbar'
 
 export default function App() {
@@ -16,6 +17,8 @@ export default function App() {
                         <Route path="/" element={<Home />} />
                         <Route path="/user/:userId" element={<UserDetail />} />
                         <Route path="/scoring" element={<Scoring />} />
+                        <Route path="/student/feedback" element={<StudentFeedback />} />
+                        <Route path="/student/feedback/:userId" element={<StudentFeedback />} />
                     </Routes>
                 </div>
 
